58b056c1aa08f725c7b5295cd35c6afcf4a24989
EnviousData <Enviousdata@gmail.com>
2021-06-28 09:06:52 +0100

    added escape key to keymap

--- _firmware/keymap.py
+++ _firmware/keymap.py
@@ -23,7 +23,7 @@ envkb.keymap = [
     ],
     [
     #Layer 1
-        KC.GRAVE, KC.F1, KC.F2, KC.F3, KC.F4, KC.F5, KC.F6, KC.F7, KC.F8, KC.F9, KC.F10, KC.F11, KC.F12, nokey, KC.BSPC, nokey, nokey,
+        KC.ESC, KC.F1, KC.F2, KC.F3, KC.F4, KC.F5, KC.F6, KC.F7, KC.F8, KC.F9, KC.F10, KC.F11, KC.F12, nokey, KC.BSPC, nokey, nokey,
         KC.TAB, nokey, KC.Q, KC.W, KC.E, KC.R, KC.T, KC.Y, KC.U, KC.I, KC.O, KC.P, KC.LBRC, KC.RBRC, KC.BSLASH, nokey, nokey,
         KC.CAPS, nokey, KC.A, KC.S, KC.D, KC.F, KC.G, KC.H, KC.J, KC.K, KC.L, KC.SCLN, KC.QUOT, KC.NUHS, KC.ENT, nokey, nokey,
         KC.LSFT, KC.NONUS_BSLASH, KC.Z, KC.X, KC.C, KC.V, KC.B, KC.N, KC.M, KC.COMM, KC.DOT, KC.SLSH, KC.RSHIFT, nokey, KC.UP, KC.F13, KC.F14,
